bc3a38a9069d0f4be96c2d85319f16229613c8bf
japahoe2 <123871745+japahoe2@users.noreply.github.com>
2024-06-02 13:08:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["Watching: The ADBR Zone","Watching: The ADBR Zone"];
 
 
 let currentIndex = 0;
